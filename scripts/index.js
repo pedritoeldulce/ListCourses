6424b6c40bf95daa94d8586c17b37a6fc43e3d7a
pedritoeldulce <franceskolyperez@gmail.com>
2021-01-01 23:56:12 -0500

    Clases de Edux

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,57 @@
 import Curso from "./classes/Course.js"
 
-const ingles = new Curso("https:drive","descripcion del curso","HTML desde cero", "../img/ingles_logo.png", 6, 23)
-/* const ingles_1 = new Curso("https:drive","descripcion del curso","HTML desde cero", "https://edteam-media.s3.amazonaws.com/courses/big/4feb100b-5d47-4daf-b421-9983c5ecdfa8.png", 6, 23)*/
+// instanciamos objetos de Curso
+const ingles_1 = new Curso("https:drive","descripcion del curso","Inglés Básico", "../img/ingles_1.png", 8, 23)
+const ingles_2 = new Curso("https:drive","descripcion del curso","Inglés Intermedio", "../img/ingles_2.jpg", 7, 23)
+const ingles_3 = new Curso("https:drive","descripcion del curso","Inglés Avanzado", "../img/ingles_3.jpg", 6, 23)
+const git = new Curso("https:drive","descripcion del curso","Inglés Avanzado", "../img/git_logo.png", 4, 23)
+const git_2 = new Curso("https:drive","descripcion del curso","Inglés Avanzado", "../img/git_logo.png", 4, 23)
 
-const element = document.getElementById("Curso")
 
-element.innerHTML = `
+const element = document.getElementById("cursos")
+
+// Imprime un objeto en el DOM
+// REcibe un ojbeto del tipo cursp
+
+function mostrarCurso(curso){
+    const hijo = document.createElement("div")
+    //hijo.classList.add("card") --> Otra forma de agregar un etiqueta Html
+
+hijo.innerHTML = `
+
+<article class=card>
+    <div class="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+        <img src="${curso.getPoster()}" alt="${curso.getNombre()}"/>
+    </div>
+    <div class="card__data s-border s-radius-br s-radius-bl s-pxy-1">
+        <h3 class="t3 s-mb-2 s-center">
+        ${curso.getNombre()} 
+        </h3>
+    </div>
+    <div class="s-border">
+    <span> Números de Clases: ${curso.getCapitulos()}</span>
+    </div>
+
+</article>
+`
+element.appendChild(hijo)
+}
+
+mostrarCurso(ingles_2)
+mostrarCurso(ingles_1)
+mostrarCurso(ingles_3)
+mostrarCurso(git)
+mostrarCurso(git_2)
+
+
+/* 
+`
 <img src="${ingles.getPoster()}" width="200em" height="100em" />
 <h3>${ingles.getNombre()} </h3>
 <span>Desripcion: ${ingles.getDescripcion()}</span>
 
 `
+*/
 
 
 /* issues: Las imagenes almacenadas en drive no se pueden utilizar como postal*/
